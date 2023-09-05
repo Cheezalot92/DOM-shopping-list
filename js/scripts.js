ed72ea75f4cd26e56a206e426a8761bb66b96b26
Brandon McAbee <brandonmcabee@Brandons-MacBook-Air.local>
2023-09-05 12:10:06 -0400

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-'use strict';
+import "./styles.css";
+
+
+document.addEventListener('DOMContentLoaded', function (){
+
+const submitItem = document.getElementById('#submitItem');
+
+function {
+    const input1 = document.getElementById("Item1").value;
+    const input2 = document.getElementById("Item2").value;
+    const input3 = document.getElementById("Item3").value;
+    const input4 = document.getElementById("Item4").value;
+
+
+
+    document.getElementById("textarea").textContent = textareaUno;
+
+    const textareaUno = `On this visit to the grocery store I need , ${input1} , ${input2} , ${input3} , & ${input4} .`
+}
+})
+submitItem.addEventListener('click' , function(event){
+    event.preventDefault()
+    const itemInput = document.querySelector("[class=input]:checked");
+    const myList = document.querySelector("myList");
+    const itemValue = itemInput.value;
+    myList.innerHTML= itemInput.value;
+    
+})
